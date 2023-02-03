6f3e30db452a6dd0034f3edcde5cc2e6a3b19b72
Nafi <saejim91@gmail.com>
2023-02-03 21:06:31 +0600

    dummy feedback added

--- feedback.php
+++ feedback.php
@@ -1,33 +1,58 @@
 <?php include './inc/header.php'; ?>
 
-				<h2>Feedback</h2>
+<?php
+	$feedback = [
+		[
+			'id'=> '1',
+			'name'=> 'Jin Yung',
+			'email'=> 'jin@xing.com',
+			'body'=> 'Putin in sum fidbag!'
+		],
+		[
+			'id'=> '2',
+			'name'=> 'Ryu',
+			'email'=> 'ryu@sf.com',
+			'body'=> 'Otashino khokorocho. FIGHT!!'
+		],
+		[
+			'id'=> '3',
+			'name'=> 'Beth',
+			'email'=> 'bethg@kiss.com',
+			'body'=> 'Beth was written by Kiss.'
+		],
+	]
+?>
 
-				<div class="card my-3">
-					<div class="card-body">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-						molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-						repellendus voluptatibus natus deserunt sed doloribus inventore,
-						totam labore maxime perferendis!
+
+				<h2>Past Feedback</h2>
+<?php
+	if(empty($feedback)):
+?>
+
+		<p class="lead mt3">There is no feedback </p>
+
+<?php
+	endif;
+?>
+
+<?php foreach($feedback as $item): ?>
+
+				<div class="card my-3 w-75">
+					<div class="card-body text-center">
+						<?php
+							echo $item['body'];
+						?>
+						<div class="text-secondary mt-2">
+							By <?php
+								echo $item['name'];
+							?>
+						</div>
 					</div>
 				</div>
 
-				<div class="card my-3">
-					<div class="card-body">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-						molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-						repellendus voluptatibus natus deserunt sed doloribus inventore,
-						totam labore maxime perferendis!
-					</div>
-				</div>
-
-				<div class="card my-3">
-					<div class="card-body">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-						molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-						repellendus voluptatibus natus deserunt sed doloribus inventore,
-						totam labore maxime perferendis!
-					</div>
-				</div>
-
+				
+<?php
+	endforeach;
+?>
 
 <?php include './inc/footer.php'; ?>
