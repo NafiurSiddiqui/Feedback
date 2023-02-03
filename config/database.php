5e57725d9d978d94c44674507f46a46b1e1a47e5
Nafi <saejim91@gmail.com>
2023-02-03 20:39:56 +0600

    DB connected

--- config/database.php
+++ config/database.php
@@ -14,3 +14,12 @@
  //CREATE CONNECTION
 
  $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+ //CHECK CONNECTION
+
+ if($conn->connect_error){
+    die('Connection Failed'. $conn->connect_error);
+ }
+
+//  echo 'DB CONNECTED '; //TEST
+
